b556962dc9b025621f69f963112f4c40bf249724
nathan desmee <nathan.desmee.etu@univ-lille.fr>
2023-11-25 12:09:32 +0100

    portfolio v1

--- script/script.js
+++ script/script.js
@@ -1,8 +1,22 @@
-function showMenu() {
+const link = document.getElementsByClassName("link");
+
+function hamburger() {
     let visibility = document.getElementById("hamburger").style.display;
     if(visibility == "none") {
         document.getElementById("hamburger").style.display = "block";
     } else {
         document.getElementById("hamburger").style.display = "none";
     }
+}
+
+function hideMenu() {
+    document.getElementById("hamburger").style.display = "none";
+}
+
+for(let l of link) {
+
+    l.addEventListener("click", function() {
+        hideMenu();
+    });
+
 }
