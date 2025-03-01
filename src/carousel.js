27bbb6fbebe6bb10a8ce0b510143f4cf84bb93cd
nathan desmee <nathan.desmee.etu@univ-lille.fr>
2025-03-01 14:38:49 +0100

    maj et correction

--- src/carousel.js
+++ src/carousel.js
@@ -82,7 +82,7 @@ function renderProject(data){
     return `<div class="w-1/3 m-auto"><img src="${data.logo}" alt="${data.logo}" class="my-3"/></div><h1 class="text-xl my-3 text-red-700">${data.title}</h1><p class="my-3">${data.date}</p><p class="my-3 text-sm">${data.description}</p><p class="my-3 text-sm">${data.skills}</p><a href="${data.link}" class="my-2 text-blue-400" >Voir le projet</a>`;
 }
 
-fetch(`/data/hmf.json`)
+fetch(`/data/jo.json`)
     .then(response => response.json())
     .then(data => {
         const projectDisplayer = document.querySelector('.projectDisplayer');
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-const aboutMeText = `Bonjour, je m'appelle Nathan Desmee et je suis actuellement en 3eme année de BUT informatique. Je suis fortement intéressé par le domaine de l'intelligence artificielle car c'est le domaine informatique qui a le plus grand potentiel selon moi. En effet, je pense que l'IA peut jouer un rôle majeur dans l'évolution de l'humanité notament dans le domaine de la santé, du confort et de l'écologie. Ainsi, j'aimerais contribuer au développement de cette technologie qui va marquer un tournant dans l'histoire.`;
-const aboutFormationText = `                                                                                        Grâce à ma formation, j'ai pu acquérir de nombreuses compétences informatiques et réaliser de nombreux projects. Avec un binôme, j'ai notamanent créé une base de données complète contenant des informations sur les jeux olympiques, j'ai ensuite analysé ces données pour produire des graphiques statistiques. Ce fut mon premier pas vers la gestion et l'analyse de données. J'ai également pu réaliser, avec un groupe de 4 personnes, un jeu vidéo de type "chasse au monstre" dans lequel les joueurs peuvent affronter des IA. Finalement, j'ai pu avoir l'opportunité de travailler en Norvège à la Norwegian University of Science and Technology dans le cadre de mon stage de fin de 2e année. Durant ce stage j'ai pu parfaire mon anglais et en apprendre plus sur les technologies du domaine du visuel.`;
+const aboutMeText = `Bonjour, je m'appelle Nathan DESMEE et je suis actuellement en 3eme année de BUT informatique. Je suis fortement intéressé par le domaine de l'intelligence artificielle car, selon moi, c'est le domaine informatique qui a le plus grand potentiel de croissance. En effet, je pense que l'IA peut jouer un rôle majeur dans l'évolution de l'humanité notamment dans le domaine de la santé, du confort et de l'écologie. Ainsi, j'aimerais contribuer au développement de cette technologie qui va marquer un tournant dans l'histoire.`;
+const aboutFormationText = `                                                                                        Grâce à ma formation, j'ai pu acquérir de nombreuses compétences informatiques et réaliser de nombreux projets. En binôme, j'ai notamment créé une base de données complète contenant des informations sur les jeux olympiques. J'ai ensuite analysé ces données pour produire des graphiques statistiques. Ce fut mon premier pas vers la gestion et l'analyse de données. J'ai également pu réaliser, de nouveau en binôme, un site web permettant de prendre des rendez-vous ce qui m'a permis d'apprendre de nouvelles compétences dans divers domaines. Finalement, j'ai eu l'opportunité de travailler en Norvège à la Norwegian University of Science and Technology dans le cadre de mon stage de fin de 2e année de BUT. Durant ce stage, j'ai pu parfaire mon anglais et en apprendre plus sur les technologies du domaine du visuel.`;
+const aboutKnowHowText = `                                                                                        Je suis capable d'une grande adaptabilité car je n'ai pas de difficulté à m'acclimater à un nouvel environnement et à me conformer à de nouvelles exigences. Je suis aussi très autonome et je peux travailler sur les tâches qui me sont confiées sans avoir besoin d'être constamment surveillé. De plus, je suis responsable et rigoureux. Ainsi, je fais toujours le maximum pour que le travail qui m'a été confié soit fait le mieux possible et dans les délais. Finalement, je suis persévérant car je n'ai pas peur des difficultés et je suis prêt à les surmonter pour atteindre mes objectifs.`;
 const links = document.querySelectorAll(".link");
 const button = document.querySelector("button");
 const hamburger = document.querySelector(".hamburger");
@@ -8,6 +9,7 @@ let visible = false;
 window.addEventListener("load", (event) => {
     const aboutMe = document.querySelector('.aboutMe');
     const aboutFormation = document.querySelector('.aboutFormation');
+    const aboutKnowHow = document.querySelector('.aboutKnowHow');
     let i = 0;
     const interval = setInterval(() => {
         if(i < aboutMeText.length){
@@ -17,9 +19,14 @@ window.addEventListener("load", (event) => {
             aboutFormation.innerHTML = aboutFormationText[0] + '|';
         }else if(i < aboutMeText.length + aboutFormationText.length){
             aboutFormation.innerHTML =  aboutFormation.innerHTML.substring(0, aboutFormation.innerHTML.length-1) + aboutFormationText[i - aboutMeText.length] + '|';
+        }else if(i == aboutMeText.length + aboutFormationText.length){
+            aboutFormation.innerHTML = aboutFormation.innerHTML.substring(0, aboutFormation.innerHTML.length-1);
+            aboutKnowHow.innerHTML = aboutKnowHowText[0] + '|';
+        }else if(i < aboutMeText.length + aboutFormationText.length + aboutKnowHowText.length){
+            aboutKnowHow.innerHTML = aboutKnowHow.innerHTML.substring(0, aboutKnowHow.innerHTML.length-1) + aboutKnowHowText[i - aboutMeText.length - aboutFormationText.length] + '|';
         }else{
             clearInterval(interval);
-            aboutFormation.innerHTML = aboutFormationText;
+            aboutKnowHow.innerHTML = aboutKnowHowText;
         }
         i++;
     }, 1);
